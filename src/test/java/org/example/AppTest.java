a96b3e5eb5bc9532b3024629ddca50eea691382a
Michael <michaeldoyon1@gmail.com>
2021-08-31 17:08:50 -0400

    Initial Commit

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,13 +1,14 @@
+/**
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Michael Doyon
+ */
 package org.example;
 
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-/**
- *  UCF COP3330 Fall 2021 Assignment 1 Solution
- *  Copyright 2021 Michael Doyon
- */
+
 public class AppTest 
 {
     /**
